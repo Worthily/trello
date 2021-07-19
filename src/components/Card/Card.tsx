91adfe8e518491351569a195b288a30af2ed200f
Worthily <koka_cola_dima2@mail.ru>
2021-07-19 23:16:09 +0600

    add cards

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,10 +1,67 @@
-import React from 'react';
-function Card() {
-  return (
-    <div>
-      <div></div>
-    </div>
-  );
+import React, { Component } from 'react';
+import commentImg from '../../assets/img/comment.png';
+import notCheckImg from '../../assets/img/viewblack.png';
+import checkImg from '../../assets/img/viewgreen.png';
+import dellImg from '../../assets/img/delete.svg';
+
+interface CardType {
+  header?: string;
+  text?: string;
+  checked?: boolean;
+  author?: string;
+}
+interface CardState {
+  header?: string;
+  text?: string;
+  checked?: boolean;
+  author?: string;
 }
 
-export default Card;
+export default class Card extends Component<CardType, CardState> {
+  // eslint-disable-next-line
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      header: props.header,
+      text: props.text,
+      author: props.author,
+      checked: props.checked,
+    };
+    this.OnChecked = this.OnChecked.bind(this);
+  }
+  OnChecked() {
+    this.setState(({ checked }) => ({
+      checked: !checked,
+    }));
+  }
+  render() {
+    const { header, text, checked, author } = this.state;
+    let btnSrc;
+    if (checked == true) {
+      btnSrc = checkImg;
+    } else {
+      btnSrc = notCheckImg;
+    }
+
+    return (
+      <div className="column__card card">
+        <h2 className="card__header">{header}</h2>
+        <p className="card__autor">{author}</p>
+        <div className="card__text-wrapper">
+          <p className="card__text">{text}</p>
+        </div>
+        <div className="card__buttons-wrapper">
+          <div onClick={this.OnChecked} className="card__checked-btn card__btn">
+            <img src={btnSrc} alt="checked" className="card__checked-img" />
+          </div>
+          <div className="card__comments-btn card__btn">
+            <img src={commentImg} alt="comment" className="card__comment-img" />
+          </div>
+          <div className="card__dell-btn card__btn">
+            <img src={dellImg} alt="delete" className="card__dell-img" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
-
+import Card from '../Card';
 import Addcardbtn from '../../ui/Add-new-card-btn';
 
-function Column({ header = 'default' }) {
+function Column({
+  cards,
+  header,
+}: {
+  // eslint-disable-next-line
+  cards: any[];
+  header: string;
+}): JSX.Element {
+  const elements = cards.map((item) => {
+    if (item) {
+      return (
+        <li key={item.id} className="column__card-item">
+          <Card
+            header={item.header}
+            text={item.text}
+            checked={item.checked}
+            author={item.author}
+          />
+        </li>
+      );
+    }
+  });
+
   return (
     <div className="column">
       <h2 className="column__header">{header}</h2>
+      <ul className="column__card-wrapper">{elements}</ul>
       <Addcardbtn />
     </div>
   );
