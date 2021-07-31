adf51752855443bc197e2f9baf77ded5c7ad9205
Worthily <koka_cola_dima2@mail.ru>
2021-07-31 08:30:22 +0600

    fixed Column

--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -1,122 +1,88 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Card from '../Card';
 import AddCardBtn from '../../ui/AddCardBtn';
 import changeImg from '../../assets/img/change.png';
 
-interface ColumnProps {
-  // eslint-disable-next-line
-  cards: any[];
-  id: string;
-  header: string;
-  OnDelete(id: string): void;
-  onCheck(id: string): void;
-  getHeader(header: string, id: string): void;
-  createCard(id: string): void;
-  onShowPopup(id: string): void;
-  commentsCount(id: string): number;
-}
-interface ColumnState {
-  header: string;
-  changeing: boolean;
-}
+// eslint-disable-next-line
+function Column(props: any) {
+  const [header, setHeader] = useState(props.header);
+  const [change, setChange] = useState(false);
 
-export default class Column extends Component<ColumnProps, ColumnState> {
-  // eslint-disable-next-line
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      changeing: false,
-      header: this.props.header,
-    };
-    this.onChangeBtnClick = this.onChangeBtnClick.bind(this);
-    this.onValueChange = this.onValueChange.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
+  function onChangeBtnClick(): void {
+    setChange(!change);
   }
 
-  onChangeBtnClick(): void {
-    this.setState(({ changeing }) => {
-      return {
-        changeing: !changeing,
-      };
-    });
+  function onValueChange(e: React.FormEvent<HTMLInputElement>): void {
+    setHeader(e.currentTarget.value);
   }
 
-  onValueChange(e: React.FormEvent<HTMLInputElement>): void {
-    this.setState({
-      header: e.currentTarget.value,
-    });
-  }
-
-  onSubmit(e: React.FormEvent) {
+  function onSubmit(e: React.FormEvent) {
     e.preventDefault();
 
-    if (this.state.header !== '') {
-      this.props.getHeader(this.state.header, this.props.id);
-      this.setState({
-        changeing: false,
-      });
+    if (header !== '') {
+      props.getHeader(header, props.id);
+      setChange(false);
     }
   }
 
-  render() {
-    const {
-      cards,
-      OnDelete,
-      onCheck,
-      createCard,
-      id,
-      onShowPopup,
-      commentsCount,
-    } = this.props;
-    let heading = (
-      <div className="column__header-wrapper">
-        <h2 className="column__header">{this.props.header}</h2>
-        <div
-          onClick={this.onChangeBtnClick}
-          className="column__header-change-btn">
-          <img src={changeImg} alt="change" />
-        </div>
+  const {
+    cards,
+    OnDelete,
+    onCheck,
+    createCard,
+    id,
+    onShowPopup,
+    commentsCount,
+  } = props;
+  let heading = (
+    <div className="column__header-wrapper">
+      <h2 className="column__header">{props.header}</h2>
+      <div onClick={onChangeBtnClick} className="column__header-change-btn">
+        <img src={changeImg} alt="change" />
       </div>
-    );
-    if (this.state.changeing) {
-      heading = (
-        <div className="column__header-wrapper">
-          <form onSubmit={this.onSubmit} className="column__form">
-            <input
-              onChange={this.onValueChange}
-              className="column__input"
-              type="text"
-              name=""
-            />
-            <button className="column__submit-btn">OK</button>
-          </form>
-        </div>
-      );
-    }
-    const elements = cards.map((item) => {
-      if (item) {
-        return (
-          <li key={item.id} className="column__card-item">
-            <Card
-              header={item.header}
-              text={item.text}
-              checked={item.checked}
-              author={item.author}
-              OnDelete={() => OnDelete(item.id)}
-              onCheck={() => onCheck(item.id)}
-              onShowPopup={() => onShowPopup(item.id)}
-              commentsCount={() => commentsCount(item.id)}
-            />
-          </li>
-        );
-      }
-    });
-    return (
-      <div className="column">
-        {heading}
-        <ul className="column__card-wrapper">{elements}</ul>
-        <AddCardBtn createCard={() => createCard(id)} />
+    </div>
+  );
+  if (change) {
+    heading = (
+      <div className="column__header-wrapper">
+        <form onSubmit={onSubmit} className="column__form">
+          <input
+            onChange={onValueChange}
+            className="column__input"
+            type="text"
+            name=""
+          />
+          <button className="column__submit-btn">OK</button>
+        </form>
       </div>
     );
   }
+  // eslint-disable-next-line
+  const elements = cards.map((item: any) => {
+    if (item) {
+      return (
+        <li key={item.id} className="column__card-item">
+          <Card
+            header={item.header}
+            text={item.text}
+            checked={item.checked}
+            author={item.author}
+            OnDelete={() => OnDelete(item.id)}
+            onCheck={() => onCheck(item.id)}
+            onShowPopup={() => onShowPopup(item.id)}
+            commentsCount={() => commentsCount(item.id)}
+          />
+        </li>
+      );
+    }
+  });
+  return (
+    <div className="column">
+      {heading}
+      <ul className="column__card-wrapper">{elements}</ul>
+      <AddCardBtn createCard={() => createCard(id)} />
+    </div>
+  );
 }
+
+export default Column;
